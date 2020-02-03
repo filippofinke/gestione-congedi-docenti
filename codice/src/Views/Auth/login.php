f9ced6fdf3de0bff241724fb19f782d8ede2d0f4
Filippo Finke <filippo.finke@samtrevano.ch>
2020-02-03 13:38:11 +0100

    Aggiunto metodo per recupero password

--- codice/src/Views/Auth/login.php
+++ codice/src/Views/Auth/login.php
@@ -7,30 +7,28 @@
 	<div class="login-wrap customscroll d-flex align-items-center flex-wrap justify-content-center pd-20">
 		<div class="login-box bg-white box-shadow pd-30 border-radius-5">
 			<h2 class="text-center mb-30">Login</h2>
-			<form>
+			<form onsubmit="doLogin(event);">
 				<div class="input-group custom input-group-lg">
-					<input id="username" type="text" class="form-control" placeholder="Username">
+					<input id="username" type="text" class="form-control" placeholder="Username" required minlength="1">
 					<div class="input-group-append custom">
 						<span class="input-group-text"><i class="fa fa-user" aria-hidden="true"></i></span>
 					</div>
 				</div>
 				<div class="input-group custom input-group-lg">
-					<input id="password" type="password" class="form-control" placeholder="**********">
+					<input id="password" type="password" class="form-control" placeholder="**********" required minlength="6">
 					<div class="input-group-append custom">
 						<span class="input-group-text"><i class="fa fa-lock" aria-hidden="true"></i></span>
 					</div>
 				</div>
 				<div id="error" class="alert alert-danger" role="alert" style="display: none;">
-					Credenziali errate.
 				</div>
 				<div id="success" class="alert alert-success" role="alert" style="display: none;">
-					Accesso eseguito!
 				</div>
 				<div class="input-group">
-					<button id="login" type="button" class="btn btn-outline-primary btn-lg btn-block">Accedi</button>
+					<button type="submit" class="btn btn-outline-primary btn-lg btn-block">Accedi</button>
 				</div>
 				<div id="forgot-password" style="display:none;">
-					<div class="forgot-password padding-top-10"><a href="/forgot-password">Password dimenticata?</a></div>
+					<div class="forgot-password padding-top-10"><a onclick="forgotPassword()">Password dimenticata?</a></div>
 				</div>
 			</form>
 		</div>
@@ -38,12 +36,13 @@
     <?php include(__DIR__ . '/../Global/script.php'); ?>
 	
 	<script>
-	$("#login").click(doLogin);
 	
-	function doLogin() {
+	function doLogin(event) {
+		event.preventDefault();
 		var username = $("#username").val();
 		var password = $("#password").val();
-		if(username.length > 0 && password.length > 0) {
+		console.log(username, password);
+		if((isValidLdapUsername(username) || isValidEmail(username)) && password.length > 0) {
 			fetch('/login', {
 				method:'post',
 				body: "username=" + username + "&password=" + password,
@@ -53,18 +52,42 @@
 			}).then((response) => {
 				if(response.status == 200) {
 					$("#error").css("display","none");
-					$("#success").css("display","block");
+					$("#success").css("display","block").text("Accesso eseguito!");
 					setTimeout(function() {
 						window.location = "/";
 					}, 500);
 				} else {
-					$("#error").css("display","block");
+					$("#error").css("display","block").text("Credenziali errate!");
 					$("#success").css("display","none");
-					if(isEmail(username)) {
+					if(isValidEmail(username)) {
 						$("#forgot-password").css("display","block");
 					}
 				}
 			})
+		}
+	}
+
+	function forgotPassword() {
+		var username = $("#username").val();
+		if(isValidEmail(username)) {
+			fetch('/forgot-password', {
+				method:'POST',
+				body: "email=" + username,
+				headers:{
+					"Content-Type":"application/x-www-form-urlencoded"
+				}
+			}).then((response) => { 
+				if(response.status == 200) {
+					$("#error").css("display","none");
+					$("#success").css("display","block").text("Email di recupero inviata!");
+				} else {
+					$("#error").css("display","block").text("Impossibile inviare una email di recupero!");
+					$("#success").css("display","none");
+				}
+			});
+		} else {
+			$("#error").css("display","block").text("Inserisci l'email!");
+			$("#success").css("display","none");
 		}
 	}
 
