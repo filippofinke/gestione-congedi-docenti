7b298c289c3b724a21d2e711666a5351bf06f342
Filippo Finke <filippo.finke@samtrevano.ch>
2020-02-03 13:37:01 +0100

    Aggiornamento struttura

--- codice/src/Models/LdapUser.php
+++ codice/src/Models/LdapUser.php
@@ -3,14 +3,16 @@ namespace FilippoFinke\Models;
 
 use FilippoFinke\Models\LdapUsers;
 use FilippoFinke\Utils\Database;
+use PDOException;
 
 /**
  * LdapUser.php
  * Classe che rappresenta un utente.
- * 
+ *
  * @author Filippo Finke
  */
-class LdapUser {
+class LdapUser
+{
 
     /**
      * L'username dell'utente.
@@ -34,57 +36,63 @@ class LdapUser {
 
     /**
      * Metodo getter per ricavare l'username dell'utente.
-     * 
+     *
      * @return String L'username dell'utente.
      */
-    public function getUsername() {
+    public function getUsername()
+    {
         return $this->username;
     }
 
     /**
      * Metodo getter per ricavare il nome dell'utente.
-     * 
+     *
      * @return String Il nome dell'utente.
      */
-    public function getName() {
+    public function getName()
+    {
         return $this->name;
     }
 
     /**
      * Metodo getter per ricavare il cognome dell'utente.
-     * 
+     *
      * @return String Il cognome dell'utente.
      */
-    public function getLastName() {
+    public function getLastName()
+    {
         return $this->lastName;
     }
 
     /**
      * Metodo getter per ricavare il permesso dell'utente.
-     * 
+     *
      * @return String Il permesso dell'utente.
      */
-    public function getPermission() {
+    public function getPermission()
+    {
         return $this->permission;
     }
 
 
     /**
      * Metodo costruttore con 4 parametri.
-     * 
+     *
      * @param $username L'username dell'utente.
      * @param $name Il nome dell'utente.
      * @param $lastName Il cognome dell'utente.
      * @param $permission Il permesso dell'utente.
      */
-    public function __construct($username, $name, $lastName) {
+    public function __construct($username, $name, $lastName)
+    {
         $this->username = $username;
         $this->name = $name;
         $this->lastName = $lastName;
         $this->setPermission();
     }
 
-    private function setPermission() {
+    private function setPermission()
+    {
         $data = LdapUsers::getByUsername($this->username);
         if ($data) {
             $this->permission = $data["permission"];
@@ -94,22 +102,26 @@ class LdapUser {
         }
     }
 
-    public function updateLastLogin() {
+    public function updateLastLogin()
+    {
         $pdo = Database::getConnection();
         $query = "UPDATE users SET last_login = CURRENT_TIMESTAMP WHERE username = :username";
         $stm = $pdo->prepare($query);
         $stm->bindParam(':username', $this->username);
-        return $stm->execute();
+        try {
+            return $stm->execute();
+        } catch (PDOException $e) {
+            return false;
+        }
     }
 
     /**
      * Metodo utilizzato per stampare le informazioni dell utente.
-     * 
+     *
      * @return String Una stringa nel formato "Nome Cognome".
      */
     public function __toString()
     {
         return $this->name." ".$this->lastName;
     }
-
 }
--- codice/src/Models/LdapUsers.php
+++ codice/src/Models/LdapUsers.php
@@ -3,29 +3,62 @@
 namespace FilippoFinke\Models;
 
 use FilippoFinke\Utils\Database;
+use PDOException;
 
-class LdapUsers {
+class LdapUsers
+{
+    public static function getAll()
+    {
+        $pdo = Database::getConnection();
+        $query = "SELECT * FROM users";
+        try {
+            $stm = $pdo->query($query);
+            return $stm->fetchAll(\PDO::FETCH_ASSOC);
+        } catch (PDOException $e) {
+            return false;
+        }
+    }
 
-    public static function getByUsername($username) {
+    public static function getByUsername($username)
+    {
         $pdo = Database::getConnection();
         $query = "SELECT * FROM users WHERE username = :username";
         $stm = $pdo->prepare($query);
         $stm->bindParam(':username', $username);
-        if($stm->execute()) {
+        try {
+            $stm->execute();
             return $stm->fetch(\PDO::FETCH_ASSOC);
-        } 
-        return false;
+        } catch (PDOException $e) {
+            return false;
+        }
     }
 
-    public static function insert($username, $name, $lastName) {
+    public static function insert($username, $name, $lastName)
+    {
         $pdo = Database::getConnection();
         $query = "INSERT INTO users(username, name, last_name) VALUES(:username, :name, :last_name)";
         $stm = $pdo->prepare($query);
         $stm->bindParam(':username', $username);
         $stm->bindParam(':name', $name);
         $stm->bindParam(':last_name', $lastName);
-        return $stm->execute();
+        try {
+            return $stm->execute();
+        } catch (PDOException $e) {
+            return false;
+        }
     }
 
-
+    public static function setPermission($username, $permission)
+    {
+        $pdo = Database::getConnection();
+        $query = "UPDATE users SET permission = :permission WHERE username = :username";
+        $stm = $pdo->prepare($query);
+        $stm->bindParam(':username', $username);
+        $stm->bindParam(':permission', $permission);
+        try {
+            return $stm->execute();
+        } catch (PDOException $e) {
+            return false;
+        }
+    }
 }
