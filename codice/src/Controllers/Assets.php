8cf508f679787dace9f3a34d2165e9556079fb41
Filippo Finke <filippo.finke@samtrevano.ch>
2020-02-03 13:32:53 +0100

    Aggiunta la risposta per richieste di immagini
    Inoltre ristrutturato il codice

--- codice/src/Controllers/Assets.php
+++ codice/src/Controllers/Assets.php
@@ -1,47 +1,49 @@
-<?php 
+<?php
 namespace FilippoFinke\Controllers;
 
-class Assets {
-
+class Assets
+{
     private const JS_FOLDER = __DIR__ . '/../Assets/js/';
     private const CSS_FOLDER = __DIR__ . '/../Assets/css/';
     private const FONTS_FOLDER = __DIR__ . '/../Assets/fonts/';
+    private const IMAGES_FOLDER = __DIR__ . '/../Assets/img/';
 
-    public static function js($request, $response) {
-        $asset = basename($request->getAttribute('asset'));
-        if(file_exists(self::JS_FOLDER.$asset)) {
-            return $response
-            ->withHeader("Content-Type","text/javascript")
-            ->withBody(file_get_contents(self::JS_FOLDER.$asset))
-            ->withStatus(200);
-        } else {
-            return $response->withStatus(404)->withText("Not found");
-        }
-    }
-
-    public static function css($request, $response) {
-        $asset = basename($request->getAttribute('asset'));
-        if(file_exists(self::CSS_FOLDER.$asset)) {
-            return $response
-            ->withHeader("Content-Type","text/css")
-            ->withBody(file_get_contents(self::CSS_FOLDER.$asset))
-            ->withStatus(200);
-        } else {
-            return $response->withStatus(404)->withText("Not found");
-        }
-    }
-
-    public static function fonts($request, $response) {
+    public static function js($request, $response)
+    {
         $asset = $request->getAttribute('asset');
-        if(file_exists(self::FONTS_FOLDER.$asset)) {
-            $mime = mime_content_type(self::FONTS_FOLDER.$asset);
+        return self::handle(self::JS_FOLDER.$asset, $response, 'text/javascript');
+    }
+
+    public static function css($request, $response)
+    {
+        $asset = $request->getAttribute('asset');
+        return self::handle(self::CSS_FOLDER.$asset, $response, 'text/css');
+    }
+
+    public static function fonts($request, $response)
+    {
+        $asset = $request->getAttribute('asset');
+        return self::handle(self::FONTS_FOLDER.$asset, $response);
+    }
+
+    public static function img($request, $response)
+    {
+        $asset = $request->getAttribute('asset');
+        return self::handle(self::IMAGES_FOLDER.$asset, $response);
+    }
+
+    private static function handle($file, $response, $type = null)
+    {
+        if (file_exists($file)) {
+            if (!$type) {
+                $type = mime_content_type($file);
+            }
             return $response
-            ->withHeader("Content-Type",$mime)
-            ->withBody(file_get_contents(self::FONTS_FOLDER.$asset))
+            ->withHeader("Content-Type", $type)
+            ->withBody(file_get_contents($file))
             ->withStatus(200);
         } else {
             return $response->withStatus(404)->withText("Not found");
         }
     }
-
 }
