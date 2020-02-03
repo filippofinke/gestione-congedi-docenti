b22e7dd5c0846b6633a38328bfd70798817d4298
Filippo Finke <filippo.finke@samtrevano.ch>
2020-02-03 13:36:21 +0100

    Aggiornamenti di struttura

--- codice/src/Models/Administrator.php
+++ codice/src/Models/Administrator.php
@@ -2,8 +2,10 @@
 namespace FilippoFinke\Models;
 
 use FilippoFinke\Utils\Database;
+use PDOException;
 
-class Administrator {
+class Administrator
+{
 
     /**
      * L'username dell'utente.
@@ -27,50 +29,55 @@ class Administrator {
 
     /**
      * Metodo getter per ricavare l'username dell'utente.
-     * 
+     *
      * @return String L'username dell'utente.
      */
-    public function getUsername() {
+    public function getUsername()
+    {
         return $this->username;
     }
 
     /**
      * Metodo getter per ricavare il nome dell'utente.
-     * 
+     *
      * @return String Il nome dell'utente.
      */
-    public function getName() {
+    public function getName()
+    {
         return $this->name;
     }
 
     /**
      * Metodo getter per ricavare il cognome dell'utente.
-     * 
+     *
      * @return String Il cognome dell'utente.
      */
-    public function getLastName() {
+    public function getLastName()
+    {
         return $this->lastName;
     }
 
     /**
      * Metodo getter per ricavare il permesso dell'utente.
-     * 
+     *
      * @return String Il permesso dell'utente.
      */
-    public function getPermission() {
+    public function getPermission()
+    {
         return $this->permission;
     }
 
 
     /**
      * Metodo costruttore con 4 parametri.
-     * 
+     *
      * @param $username L'username dell'utente.
      * @param $name Il nome dell'utente.
      * @param $lastName Il cognome dell'utente.
      * @param $permission Il permesso dell'utente.
      */
-    public function __construct($username, $name, $lastName) {
+    public function __construct($username, $name, $lastName)
+    {
         $this->username = $username;
         $this->name = $name;
         $this->lastName = $lastName;
@@ -78,12 +85,16 @@ class Administrator {
     }
 
 
-    public function updateLastLogin() {
+    public function updateLastLogin()
+    {
         $pdo = Database::getConnection();
         $query = "UPDATE administrators SET last_login = CURRENT_TIMESTAMP WHERE email = :email";
         $stm = $pdo->prepare($query);
         $stm->bindParam(':email', $this->username);
-        return $stm->execute();
+        try {
+            return $stm->execute();
+        } catch (PDOException $e) {
+            return false;
+        }
     }
-
 }
