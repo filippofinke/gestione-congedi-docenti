126494e438fc764fdcd379191c7c9798dc25b3e3
Filippo Finke <filippo.finke@samtrevano.ch>
2020-02-03 13:38:26 +0100

    Aggiunta pagina principale per utenti LDAP

---
+++ codice/src/Views/Dashboard/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <?php include(__DIR__ . '/../Global/head.php'); ?>
+</head>
+<body>
+    <?php include(__DIR__ . '/../Global/header.php'); ?>
+	<?php include(__DIR__ . '/../Global/sidebar.php'); ?>
+	<div class="main-container">
+		<div class="pd-ltr-20 xs-pd-20-10">
+			<div class="min-height-200px">
+				<div class="page-header">
+					<div class="row">
+						<div class="col-md-6 col-sm-12">
+							<div class="title">
+								<h4>Gestione congedi</h4>
+							</div>
+							<nav aria-label="breadcrumb" role="navigation">
+								<ol class="breadcrumb">
+									<li class="breadcrumb-item"><a href="/dashboard">Dashboard</a></li>
+									<li class="breadcrumb-item active" aria-current="page">Home</li>
+								</ol>
+							</nav>
+						</div>
+					</div>
+				</div>
+				<div class="pd-20 bg-white border-radius-4 box-shadow mb-30">
+					<div class="row">
+						
+					</div>
+				</div>
+			</div>
+			<?php include(__DIR__ . '/../Global/footer.php'); ?>
+		</div>
+	</div>
+    <?php include(__DIR__ . '/../Global/script.php'); ?>
+	<script src="assets/js/notify.js"></script>
+</body>
+</html>
