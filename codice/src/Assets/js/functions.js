bcad6b5c9523df26826fbbc3a550ea954a66e200
Filippo Finke <filippo.finke@samtrevano.ch>
2020-02-03 13:31:53 +0100

    Aggiunta libreria DataTables

--- codice/src/Assets/js/functions.js
+++ codice/src/Assets/js/functions.js
@@ -1,4 +1,25 @@
-function isEmail(email) {
+function isValidEmail(email) {
     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
+}
+
+function isValidName(name) {
+    return isValidAlphabetAndAccents(name, 20);
+}
+
+function isValidLastName() {
+    return isValidAlphabetAndAccents(name, 20);
+}
+
+function isValidLdapUsername(username) {
+    return username.length > 0 && username.length <= 20;
+}
+
+function isValidPassword(password) {
+    return password.length >= 6;
+}
+
+function isValidAlphabetAndAccents(text, max, min = 1) {
+    var re = "/^[A-Za-zÀ-ÖØ-öø-ÿ ]{" + min + "," + max + "}$/";
+    return re.test(text);
 }
