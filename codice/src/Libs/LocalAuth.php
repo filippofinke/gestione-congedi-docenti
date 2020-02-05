ff6bbaaa56bdd3d9231a6421f9a51eb200626880
Filippo Finke <filippo.finke@samtrevano.ch>
2020-02-05 16:17:31 +0100

    Fix stile e aggiunta commenti

--- codice/src/Libs/LocalAuth.php
+++ codice/src/Libs/LocalAuth.php
@@ -1,21 +1,35 @@
 <?php
 
 namespace FilippoFinke\Libs;
+
 use FilippoFinke\Models\Administrator;
 use FilippoFinke\Models\Administrators;
 
+/**
+ * LocalAuth.php
+ * Classe utilizzata per gestire il login locale.
+ *
+ * @author Filippo Finke
+ */
 class LocalAuth
 {
+    /**
+     * Metodo utilizzato per provare ad eseguire il login locale.
+     *
+     * @param $email L'email da utilizzare.
+     * @param $password La password da utilizzare.
+     * @param $bypass Se utilizzare oppure no la password.
+     * @return bool True se il login è riuscito altrimenti false.
+     */
     public static function login($email, $password, $bypass = false)
     {
         $user = Administrators::getByEmail($email);
-        if ($user) {
-            if (password_verify($password, $user["password"]) || $bypass) {
-                if($user["last_login"] == null) {
-                    $_SESSION["force_reset_password"] = true;
-                }
-                return new Administrator($email, $user["name"], $user["last_name"]);
+        if ($user && password_verify($password, $user["password"]) || $bypass) {
+            // Controllo se l'utente è nuovo e quindi deve impostare la nuova password.
+            if ($user["last_login"] == null) {
+                $_SESSION["force_reset_password"] = true;
             }
+            return new Administrator($email, $user["name"], $user["last_name"]);
         }
         return false;
     }
