60bd6ff003b1e55eaa023f31f3d1137082fc7d1f
Filippo Finke <filippo.finke@samtrevano.ch>
2020-02-05 16:25:07 +0100

    Aggiunta commenti

--- codice/src/Libs/Session.php
+++ codice/src/Libs/Session.php
@@ -1,13 +1,29 @@
 <?php
 namespace FilippoFinke\Libs;
 
+/**
+ * Session.php
+ * Classe utilizzata per gestire la sessione corrente.
+ *
+ * @author Filippo Finke
+ */
 class Session
 {
+    /**
+     * Metodo utilizzato per controllare se un utente ha eseguito il login oppure no.
+     *
+     * @return bool True se è autenticato altrimenti false.
+     */
     public static function isAuthenticated()
     {
         return (isset($_SESSION["authenticated"]) && $_SESSION["authenticated"] == true);
     }
 
+    /**
+     * Metodo utilizzato per autenticare l'utente corrente.
+     *
+     *  @param $data Un array contenente i parametri da salvare.
+     */
     public static function authenticate($data = null)
     {
         $_SESSION["authenticated"] = true;
@@ -18,6 +34,9 @@ class Session
         }
     }
 
+    /**
+     * Metodo utilizzato per eseguire la disconnessione.
+     */
     public static function logout()
     {
         foreach ($_SESSION as $key => $value) {
@@ -26,24 +45,44 @@ class Session
         session_destroy();
     }
 
+    /**
+     * Metodo utilizzato per verificare se un utente è un docente.
+     *
+     * @return bool True se appartiene al gruppo altrimenti false.
+     */
     public static function isTeacher()
     {
         $permissions = ["Docente", "Segreteria", "Vice direzione", "Direzione"];
         return in_array($_SESSION["permission"], $permissions);
     }
 
+    /**
+     * Metodo utilizzato per verificare se un utente fa parte della segreteria.
+     *
+     * @return bool True se appartiene al gruppo altrimenti false.
+     */
     public static function isSecretary()
     {
         $permissions = ["Segreteria", "Vice direzione", "Direzione"];
         return in_array($_SESSION["permission"], $permissions);
     }
 
+    /**
+     * Metodo utilizzato per verificare se un utente fa parte della direzione.
+     *
+     * @return bool True se appartiene al gruppo altrimenti false.
+     */
     public static function isAdministration()
     {
         $permissions = ["Vice direzione", "Direzione"];
         return in_array($_SESSION["permission"], $permissions);
     }
 
+    /**
+     * Metodo utilizzato per verificare se un utente è amministratore.
+     *
+     * @return bool True se appartiene al gruppo altrimenti false.
+     */
     public static function isAdministrator()
     {
         return $_SESSION["permission"] == "Administrator";
