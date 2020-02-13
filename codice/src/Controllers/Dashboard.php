a64822e6daaeaf16f2cc46bb80659065d2079f22
Filippo Finke <filippo.finke@samtrevano.ch>
2020-02-13 15:48:45 +0100

    Aggiunta distinzione modalità visualizzazione e non

--- codice/src/Controllers/Dashboard.php
+++ codice/src/Controllers/Dashboard.php
@@ -1,29 +1,56 @@
 <?php
 namespace FilippoFinke\Controllers;
 
-use FilippoFinke\Models\Container;
+use FilippoFinke\Models\LdapUsers;
 use FilippoFinke\Models\Reasons;
 use FilippoFinke\Models\Requests;
-use FilippoFinke\Models\RequestStatus;
+use FilippoFinke\Models\Substitutes;
 
 class Dashboard
 {
     public static function index($request, $response)
     {
         $reasons = Reasons::getAll();
-        return $response->render(
-            __DIR__ . '/../Views/Dashboard/index.php',
-            array('reasons' => $reasons)
-        );
+        $id = $request->getAttribute("id");
+        if ($id) {
+            $request = Requests::getById($id);
+            if ($request) {
+                $toCheck = Reasons::getByRequestId($request["id"]);
+                $mapped = array_map(function ($e) {
+                    return $e["id"];
+                }, $toCheck);
+                return $response->render(
+                    __DIR__ . '/../Views/Dashboard/index.php',
+                    array(
+                        'reasons' => $reasons,
+                        'request' => array(
+                            'request' => $request,
+                            'user' => LdapUsers::getByUsername($request["username"]),
+                            'reasons' => $mapped,
+                            'substitutes' => Substitutes::getByRequestId($request["id"])
+                        )
+                    )
+                );
+            } else {
+                return $response->redirect("/dashboard/secretariat");
+            }
+        } else {
+            return $response->render(
+                __DIR__ . '/../Views/Dashboard/index.php',
+                array('reasons' => $reasons)
+            );
+        }
     }
 
-    public static function sent($request, $response) {
+    public static function sent($request, $response)
+    {
         return $response->render(
             __DIR__ . '/../Views/Dashboard/sent.php'
         );
     }
 
-    public static function secretariat($request, $response) {
+    public static function secretariat($request, $response)
+    {
         return $response->render(
             __DIR__ . '/../Views/Dashboard/secretariat.php'
         );
