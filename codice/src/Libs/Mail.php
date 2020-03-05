aec58f260a8dc4de8d14450a1b124d635d459eb1
Filippo Finke <filippo.finke@samtrevano.ch>
2020-03-05 15:20:08 +0100

    Aggiunta la possibilità di inviare file

--- codice/src/Libs/Mail.php
+++ codice/src/Libs/Mail.php
@@ -16,7 +16,7 @@ class Mail
 
     /**
      * Metodo setter per l'email dal quale inviare i messaggi.
-     * 
+     *
      * @param $fromEmail L'email.
      */
     public static function setFromEmail($fromEmail)
@@ -26,18 +26,38 @@ class Mail
 
     /**
      * Metodo per inviare email.
-     * 
+     *
      * @param $to L'email al quale inviare le email.
      * @param $subject Il titolo dell'email.
      * @param $body Il contenuto dell'email.
+     * @param $file Il contenuto del file.
+     * @param $filename Il nome del file da inviare.
      * @return bool True se è stata inviata altrimenti false.
      */
-    public static function send($to, $subject, $body)
+    public static function send($to, $subject, $message, $file = null, $filename = null)
     {
-        $header = "From: ".self::$fromEmail."\r\n";
-        $header.= "MIME-Version: 1.0\r\n";
-        $header.= "Content-Type: text/html; charset=utf-8\r\n";
-        $header.= "X-Priority: 1\r\n";
-        return mail($to, $subject, $body, $header);
+        $separator = md5(time());
+        $eol = "\r\n";
+        $headers = "From: name <".self::$fromEmail.">" . $eol;
+        $headers .= "MIME-Version: 1.0" . $eol;
+        $headers .= "Content-Type: multipart/mixed; boundary=\"" . $separator . "\"" . $eol;
+        $headers .= "Content-Transfer-Encoding: 7bit" . $eol;
+        $headers .= "This is a MIME encoded message." . $eol;
+
+        $body = "--" . $separator . $eol;
+        $body .= "Content-Type: text/html; charset=\"utf-8\"" . $eol;
+        $body .= $message . $eol .  $eol;
+
+        if ($file && $filename) {
+            $content = chunk_split(base64_encode($file));
+            $body .= "--" . $separator . $eol;
+            $body .= "Content-Type: application/octet-stream; name=\"" . $filename . "\"" . $eol;
+            $body .= "Content-Transfer-Encoding: base64" . $eol;
+            $body .= "Content-Disposition: attachment" . $eol;
+            $body .= $content . $eol . $eol;
+            $body .= "--" . $separator . "--";
+        }
+        
+        return mail($to, $subject, $body, $headers);
     }
 }
