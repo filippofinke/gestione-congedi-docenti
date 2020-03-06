d9b57dd909b93f2c30cd64f09ae020aee2e717e1
Filippo Finke <filippo.finke@samtrevano.ch>
2020-03-06 13:42:38 +0100

    Aggiornato invio email

--- codice/src/Libs/Mail.php
+++ codice/src/Libs/Mail.php
@@ -34,28 +34,35 @@ class Mail
      * @param $filename Il nome del file da inviare.
      * @return bool True se è stata inviata altrimenti false.
      */
-    public static function send($to, $subject, $message, $file = null, $filename = null)
+    public static function send($to, $subject, $message, $content = null, $filename = null)
     {
         $separator = md5(time());
         $eol = "\r\n";
-        $headers = "From: <".self::$fromEmail.">" . $eol;
+
+        // header principale
+        $headers = "From: <test@test.com>" . $eol;
         $headers .= "MIME-Version: 1.0" . $eol;
         $headers .= "Content-Type: multipart/mixed; boundary=\"" . $separator . "\"" . $eol;
+        $headers .= "Content-Transfer-Encoding: 7bit" . $eol;
+        $headers .= "This is a MIME encoded message." . $eol;
 
+        // messaggio
         $body = "--" . $separator . $eol;
-        $body .= "Content-Type: text/html; charset=\"utf-8\"" . $eol;
-        $body .= $message . $eol .  $eol;
-        $body .= "--" . $separator . $eol;
+        $body .= "Content-Type: text/html; charset=\"iso-8859-1\"" . $eol;
+        $body .= "Content-Transfer-Encoding: 8bit" . $eol;
+        $body .= $message . $eol . $eol;
 
-        if ($file && $filename) {
-            $content = chunk_split(base64_encode($file));
+        if ($content && $filename) {
+            // allegati
+            $content = chunk_split(base64_encode($content));
+            $body .= "--" . $separator . $eol;
             $body .= "Content-Type: application/octet-stream; name=\"" . $filename . "\"" . $eol;
             $body .= "Content-Transfer-Encoding: base64" . $eol;
             $body .= "Content-Disposition: attachment" . $eol;
             $body .= $content . $eol . $eol;
             $body .= "--" . $separator . "--";
         }
-        
+
         return mail($to, $subject, $body, $headers);
     }
 }
